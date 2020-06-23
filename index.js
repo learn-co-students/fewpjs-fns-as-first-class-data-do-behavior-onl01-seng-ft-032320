239166d0e5ed049bcc97fde23d80da0aea486cd6
Diane Crame <dcrame@icloud.com>
2020-06-22 19:07:54 -0700

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let parsedTime = time.split(':').join('');
+
+  if (parsedTime < 1200) {
+  return 'Good Morning'
+  } else if (parsedTime > 1200 && parsedTime < 1659) {
+  return 'Good Afternoon'
+  } else if (parsedTime > 1700) {
+  return 'Good Evening'
+  };
+};
+
+function displayMessage(string) {
+  
+  document.getElementById("greeting").innerText = `${string}`;
+  
+};
