1d52da64b6a38fefbd1e3b70af3b3ef2420f2b8e
ferrisbueller66 <github email address>
2020-06-22 21:31:03 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+let c = time.split(":")
+let d = parseInt(c[0])
+if (d < 12) {
+  return "Good Morning"
+  }
+else if (d >= 17){
+    return "Good Evening"
+  }
+ else {
+   return "Good Afternoon" 
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(testContent){
+  let content = document.getElementById('greeting')
+  content.innerText = testContent
+}
