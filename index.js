85e56b081f2250ccef72ed51149ef05825a5621b
nisticism <nistacular@gmail.com>
2020-07-07 21:11:05 -0700

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(greeting) {
+  let times = greeting.split(':');
+  if (parseInt(times[0], 10) < 12) {
+    return "Good Morning"
+  }
+  
+  if (parseInt(times[0], 10) > 12 && parseInt(times[0]) < 17) {
+    return "Good Afternoon"
+  }
+
+  return "Good Evening"
+
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  document.querySelector("h1").innerText = message
+
+}
